482b759f972898315af5e65b4174b25669506f92
Diego Ortiz <dao.d18@gmail.com>
2018-05-16 11:52:15 -0300

    Plugins
    Desarrollo de plugins (CRM)

---
+++ Biblioteca/Plugin.ValidarAlquiler/ValidarAlquiler.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+
+namespace Plugin.ValidarAlquiler
+{
+    public class ValidarAlquiler : IPlugin
+    {
+        #region Secure/Unsecure Configuration Setup
+        private string _secureConfig = null;
+        private string _unsecureConfig = null;
+
+        public ValidarAlquiler(string unsecureConfig, string secureConfig)
+        {
+            _secureConfig = secureConfig;
+            _unsecureConfig = unsecureConfig;
+        }
+        #endregion
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+
+
+            Entity entity = (Entity)context.InputParameters["Target"];
+
+            if (entity.LogicalName.Equals("dao_alquiler"));
+            {
+                Guid idSocioEntidad = ((EntityReference)entity.Attributes["dao_socioid"]).Id;
+                Guid idBibliotecaEntidad = ((EntityReference)entity.Attributes["dao_bibliotecaid"]).Id;
+                Guid idLibroEntidad = ((EntityReference)entity.Attributes["dao_libroid"]).Id;
+
+                ////Query Expression
+                //  Validar la existencia de la relacion Socio - Libro
+
+                var ConsultaSocioLibro = new QueryExpression("dao_alquiler");
+                ConsultaSocioLibro.TopCount = 1;
+                ConsultaSocioLibro.NoLock = true;
+                ConsultaSocioLibro.ColumnSet.AddColumns("dao_socioid", "dao_libroid");
+                ConsultaSocioLibro.Criteria = new FilterExpression();
+                ConsultaSocioLibro.Criteria.AddCondition("dao_socioid", ConditionOperator.Equal, idSocioEntidad);
+                ConsultaSocioLibro.Criteria.AddCondition("dao_libroid", ConditionOperator.Equal, idLibroEntidad);
+
+                EntityCollection RetrieveConsultaSocioLibro = service.RetrieveMultiple(ConsultaSocioLibro);
+
+                if (RetrieveConsultaSocioLibro.Entities.Count ==1)
+                {
+                    throw new InvalidPluginExecutionException("*** El socio ya tiene en alquier este libro ***");
+                }
+
+                //validar unidades disponibles en la entidad BibliotecaLibro
+
+                String consultaStockDisponible = @"<fetch top='1' no-lock='true' >
+                                                  <entity name='dao_bibliotecalibro' >
+                                                    <attribute name='dao_unidades' />
+                                                    <filter type='and' >
+                                                      <condition attribute='dao_bibliotecaid' operator='eq' value='" + idLibroEntidad + @"' />
+                                                      <condition attribute='dao_libroid' operator='eq' value='" + idBibliotecaEntidad  + @"' />
+                                                    </filter>
+                                                  </entity>
+                                                </fetch>";
+
+                FetchExpression stockDisponible = new FetchExpression(consultaStockDisponible);
+                var resultadoStockDisponible = service.RetrieveMultiple(stockDisponible);
+
+                var val = resultadoStockDisponible.Entities;
+
+                if ((int)val.Count == 0 || val == null)
+                {
+                    throw new InvalidPluginExecutionException("*** No existe la relación entre el Libro y la Biblioteca seleccionados ***");
+
+                }else if ((int)val[0].Attributes["dao_unidades"] <= 0)
+                {
+                    throw new InvalidPluginExecutionException("*** No hay unidades disponibles ***");
+                }
+            }
+        }
+    }
+}
+
---
+++ Biblioteca/Plugin.ValidarNombreBiblioteca/ValidarNombreBiblioteca.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+
+namespace Plugin.ValidarNombreBiblioteca
+{
+    public class ValidarNombreBiblioteca : IPlugin
+    {
+        #region Secure/Unsecure Configuration Setup
+        private string _secureConfig = null;
+        private string _unsecureConfig = null;
+
+        public ValidarNombreBiblioteca(string unsecureConfig, string secureConfig)
+        {
+            _secureConfig = secureConfig;
+            _unsecureConfig = unsecureConfig;
+        }
+        #endregion
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+
+            //Identificar el evento ejecutado
+            var evento = context.MessageName.ToLower();
+
+            Entity entity = (Entity)context.InputParameters["Target"];
+
+            if (entity.LogicalName.Equals("dao_biblioteca") && entity.Attributes.Contains("dao_nombre"))
+            {
+                //Captura del nombre de la biblioteca que se digito 
+                var bibliotecaDigitada = entity.Attributes["dao_nombre"].ToString().ToLower();
+
+                ////QueryExpression
+                var consultaBiblioteca = new QueryExpression("dao_biblioteca");
+                consultaBiblioteca.NoLock = true;
+                consultaBiblioteca.TopCount = 1;
+
+                //Evento Create
+                if (evento.Equals("create"))
+                {
+                    consultaBiblioteca.ColumnSet.AddColumns( "dao_nombre");
+                    consultaBiblioteca.Criteria.AddCondition("dao_nombre", ConditionOperator.Equal, bibliotecaDigitada);
+
+                    EntityCollection retrieveConsultaBiblioteca = service.RetrieveMultiple(consultaBiblioteca);
+
+                    //// Si la consulta obtuvo al menos una entidad, hay existencia de un nombre ya utilizado
+                    if (retrieveConsultaBiblioteca.Entities.Count > 0)
+                    {
+                        throw new InvalidPluginExecutionException("*** Nombre ya existente ***");
+                    }
+                }//Evento Update
+                else if (evento.Equals("update"))
+                {
+                    consultaBiblioteca.ColumnSet.AddColumns("dao_bibliotecaid", "dao_nombre");
+                    consultaBiblioteca.Criteria.AddCondition("dao_nombre", ConditionOperator.Equal, bibliotecaDigitada);
+
+                    EntityCollection retrieveConsultaBiblioteca = service.RetrieveMultiple(consultaBiblioteca);
+
+                    //// Si la consulta obtuvo al menos una entidad, hay existencia de un nombre ya utilizado
+                    //   Se comparan los GUID obtenidos con el fin de verificar si la entidad contexto a modificar  
+                    //   difiere de la entidad conusultada impidiendo asi el Update
+                    if (retrieveConsultaBiblioteca.Entities.Count > 0)
+                    {
+                        ////Captura de Identificadores Unicos
+                        // GUID de la entidad resultante de la consulta: Existencia de una biblioteca con el nombre digitado
+                        Guid idConsultado = (Guid)retrieveConsultaBiblioteca.Entities[0].Attributes["dao_bibliotecaid"];
+                        // GUID De la entidad contexto
+                        Guid idEntity = (Guid)entity.Attributes["dao_bibliotecaid"];
+
+                        if (idConsultado != idEntity)
+                        {
+                            throw new InvalidPluginExecutionException("*** La Biblioteca ya existe con ese nombre ***");
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
